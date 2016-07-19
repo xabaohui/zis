4a85f6c17f0809f3b46c3d12a3f381789884489a
Lv Bin <290479238@qq.com>
2016-07-19 15:04:36 +0800

    采购API优化：采购员名称不能包含回车

--- src/main/java/com/zis/api/ApiPurchasedAmountAddAction.java
+++ src/main/java/com/zis/api/ApiPurchasedAmountAddAction.java
@@ -114,6 +114,7 @@ public class ApiPurchasedAmountAddAction extends BaseApiAction {
 			return "操作员不能为空";
 		}
 		// 操作员名称中带回车不允许提交
+		operator = operator.trim();
 		if(operator.contains("\n")) {
 			return "操作员名称中不能含有回车等特殊符号";
 		}
